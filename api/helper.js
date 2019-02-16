c93826e33c5523bdbfb38c37f751091cc7791d63
KirianCaumes <kirian.caumes@ynov.com>
2019-02-16 12:21:47 +0100

    improve api

--- api/helper.js
+++ api/helper.js
@@ -3,32 +3,15 @@ const HTMLParser = require('node-html-parser');
 const titleRegexp = /(.+):(.+) Lyrics.+/;
 
 class Helper {
+
     /**
-    * Get url of the lyrics by searching
+    * Get lyrics from a song and a band
+    * @param {String} bandName
     * @param {String} songName
     * @returns {String}
     */
-    static async getLyricUrl(data) {
-        return await rp(`http://lyrics.wikia.com/wiki/Special:Search?search=${data.replace(/\s/g, "+")}`)
-            .then((body) => {
-                const parsedBandItemBody = HTMLParser.parse(body);
-                return parsedBandItemBody
-                    .querySelectorAll('ul.Results h1 a')
-                    .map(item => item.attributes.href)
-                    .filter(href => href.split(':').length > 2)[0]
-            })
-            .catch((e) => {
-                console.log(e)
-            })
-    }
-
-    /**
-    * Get lyrics from a song url
-    * @param {String} songUrl
-    * @returns {String}
-    */
-    static async getLyrics(data) {
-        return await rp(data)
+    static async getLyrics(band, song) {
+        return await rp(`http://lyrics.wikia.com/wiki/${this.toCapitalize(band).replace(/\s/g, "_")}:${this.toCapitalize(song).replace(/\s/g, "_")}`)
             .then((body) => {
                 const parsedItemBody = HTMLParser.parse(body);
                 const htmlTitle = parsedItemBody.querySelector('title').text;
@@ -51,44 +34,43 @@ class Helper {
                 }
             })
             .catch((e) => {
-                console.log(e)
+                throw "Lyrics not found"
             })
     }
 
     /**
-    * Get a band url by searching
+    * Get a random song from a band
     * @param {String} bandName
     * @returns {String}
     */
-    static async getBandUrl(data) {
-        return await rp(`http://lyrics.wikia.com/wiki/Special:Search?search=${data.replace(/\s/g, "+")}`)
+    static async getRandomSongNameByBand(band) {
+        return await rp(`http://lyrics.wikia.com/wiki/${this.toCapitalize(band).replace(/\s/g, "_")}`)
             .then((body) => {
-                const parsedBody = HTMLParser.parse(body)
-                return parsedBody
-                    .querySelectorAll('ul.Results h1 a')
-                    .map(item => item.attributes.href)
-                    .filter(href => href.split(':').length < 3)[0]
-
+                const parsedBandItemBody = HTMLParser.parse(body);
+                const songsList = parsedBandItemBody.querySelectorAll('ol li b a').map(item => item.attributes.title).filter(x => !x.includes("(page does not exist)"))
+                return songsList[Math.floor(Math.random() * songsList.length) + 0].split(':')[1];
             })
             .catch((e) => {
-                console.log(e)
+                throw "Band or random song not found"
             })
     }
 
     /**
-    * Get a random song from a bandUrl
-    * @param {String} bandUrl
+    * Get a random song from an album's band
+    * @param {String} bandName
+    * @param {String} albumName
+    * @param {String} releaseYear
     * @returns {String}
     */
-    static async getRandomLyricUrl(data) {
-        return await rp(data)
+    static async getRandomSongNameByAlbum(band, album, year) {
+        return await rp(`http://lyrics.wikia.com/wiki/${this.toCapitalize(band).replace(/\s/g, "_")}:${this.toCapitalize(album).replace(/\s/g, "_")}_(${year})`)
             .then((body) => {
                 const parsedBandItemBody = HTMLParser.parse(body);
-                const songsList = parsedBandItemBody.querySelectorAll('ol li b a').map(item => item.attributes.href)
-                return "http://lyrics.wikia.com/"+songsList[Math.floor(Math.random() * songsList.length) + 0];
+                const songsList = parsedBandItemBody.querySelectorAll('ol li b a').map(item => item.attributes.title).filter(x => !x.includes("(page does not exist)"))
+                return songsList[Math.floor(Math.random() * songsList.length) + 0].split(':')[1];
             })
             .catch((e) => {
-                console.log(e)
+                throw "Band, album or random song not found"
             })
     }
 
@@ -104,8 +86,12 @@ class Helper {
                 return JSON.parse(body)[0].map(x => x[0]).join('')
             })
             .catch((e) => {
-                console.log(e)
+                throw "Translation failed"
             })
+    }
+
+    static toCapitalize(text) {
+        return text.toLowerCase().split(' ').map((s) => s.charAt(0).toUpperCase() + s.substring(1)).join(' ');
     }
 }
 
--- api/index.js
+++ api/index.js
@@ -9,11 +9,34 @@ const helper = require('./helper')
 
 app.use(cors())
 
-//ex: http://localhost:5000/api/song/byname/fear%20of%20the%20dark
-app.get('/api/song/byname/:name', (req, res) => {
-    helper.getLyricUrl(req.params.name)
-        .then((lyricUrl) => {
-            helper.getLyrics(lyricUrl)
+//ex: http://localhost:5000/api/song/byname?song=abnegation&band=in%20flames
+app.get('/api/song/byname', (req, res) => {
+    if (!req.query.band || !req.query.song) res.json({ error: "Arguments SONG and BAND are required" })
+    helper.getLyrics(req.query.band, req.query.song)
+        .then((song) => {
+            helper.getTranslate(song.lyrics)
+                .then((lyricsTranslated) => {
+                    res.json(
+                        {
+                            lyricsTranslated: lyricsTranslated,
+                            lyrics: song.lyrics,
+                            song: song.song,
+                            artist: song.artist,
+                            album: song.albums
+                        }
+                    );
+                })
+                .catch((e) => res.json({ error: e }))
+        })
+        .catch((e) => res.json({ error: e }))
+});
+
+//ex: http://localhost:5000/api/song/byname?band=in%20flames
+app.get('/api/song/byband', (req, res) => {
+    if (!req.query.band) res.json({ error: "Argument BAND is required" })
+    helper.getRandomSongNameByBand(req.query.band)
+        .then((songName) => {
+            helper.getLyrics(req.query.band, songName)
                 .then((song) => {
                     helper.getTranslate(song.lyrics)
                         .then((lyricsTranslated) => {
@@ -27,37 +50,37 @@ app.get('/api/song/byname/:name', (req, res) => {
                                 }
                             );
                         })
+                        .catch((e) => res.json({ error: e }))
                 })
+                .catch((e) => res.json({ error: e }))
         })
+        .catch((e) => res.json({ error: e }))
 });
 
-//ex: http://localhost:5000/api/song/byband/system%20of%20a%20down
-app.get('/api/song/byband/:name', (req, res) => {
-    helper.getBandUrl(req.params.name)
-        .then((bandUrl) => {
-            helper.getRandomLyricUrl(bandUrl)
-                .then((lyricUrl) => {
-                    helper.getLyrics(lyricUrl)
-                        .then((song) => {
-                            helper.getTranslate(song.lyrics)
-                                .then((lyricsTranslated) => {
-                                    res.json(
-                                        {
-                                            lyricsTranslated: lyricsTranslated,
-                                            lyrics: song.lyrics,
-                                            song: song.song,
-                                            artist: song.artist,
-                                            album: song.albums
-                                        }
-                                    );
-                                })
-                                .catch((e) => res.json({ error: "Lyrics not found" }))
+//ex: http://localhost:5000/api/song/byalbum?band=in%20flames&album=come clarity&year=2006
+app.get('/api/song/byalbum', (req, res) => {
+    if (!req.query.band || !req.query.album || !req.query.year) res.json({ error: "Arguments BAND, ALBUM and YEAR are required" })
+    helper.getRandomSongNameByAlbum(req.query.band, req.query.album, req.query.year)
+        .then((songName) => {
+            helper.getLyrics(req.query.band, songName)
+                .then((song) => {
+                    helper.getTranslate(song.lyrics)
+                        .then((lyricsTranslated) => {
+                            res.json(
+                                {
+                                    lyricsTranslated: lyricsTranslated,
+                                    lyrics: song.lyrics,
+                                    song: song.song,
+                                    artist: song.artist,
+                                    album: song.albums
+                                }
+                            );
                         })
-                        .catch((e) => res.json({ error: "Random lyrics not found" }))
+                        .catch((e) => res.json({ error: e }))
                 })
-                .catch((e) => res.json({ error: "Band not found" }))
+                .catch((e) => res.json({ error: e }))
         })
-        .catch((e) => res.json({ error: "Band not found" }))
+        .catch((e) => res.json({ error: e }))
 });
 
 app.get('*', (req, res) => {
